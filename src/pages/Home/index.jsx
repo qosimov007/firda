e78c0f3575dcc67e61a49d382532d12de83829fd
qosimov007 <mirjalolq274@gmail.com>
2024-04-20 20:45:25 +0200

    first commit

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import firda from "../../img/fir.jpg"
+
+function Home() {
+
+  return (
+    <>
+      <p>Ro'yxatdan otganing bilan hurshanman endi telegraming vizlom </p>
+      <img width={400} src={firda} alt="" />
+    </>
+  );
+}
+
+export default Home;
